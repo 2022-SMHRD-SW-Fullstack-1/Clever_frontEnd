2b3ef9807ab470eeaa0b413d95eac956ccd77fbd
ahaelim <limahae1015@gmail.com>
2023-01-10 14:41:41 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./layout/Header";
+import Group from "./pages/Group/Group";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <Routes>
+        <Route path="/group" element={<Group />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "../image/logo2.png";
-
+import "./Header.scss";
 const Header = () => {
   return (
     <div className="header">
-      <img src={logo} width="200px" />
+      <img src={logo} className="logo" />
     </div>
   );
 };
