61d6946c8b3b383fbd88a48c10b584faacf55346
ahaelim <limahae1015@gmail.com>
2023-01-10 14:19:13 +0900

    폴더나누기

---
+++ src/layout/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../image/logo2.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logo} width="200px" />
+    </div>
+  );
+};
+
+export default Header;
