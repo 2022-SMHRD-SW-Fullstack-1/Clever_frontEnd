5da7822992454336613ebdaa41f504a33e2241ec
ahaelim <limahae1015@gmail.com>
2023-01-20 10:58:31 +0900

    카테고리 목록 띄우기

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import AddBoardCate from "./AddBoardCate";
+import AddBoardCate from "./Category/AddBoardCate";
 import styles from "./Board.module.scss";
 import add from "../../image/add.png";
 import axios from "axios";
@@ -25,11 +25,12 @@ const Board = () => {
       .post("/board/getcategory", groupInfo)
       .then((res) => {
         setCateList(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [cateList]);
+  }, []);
 
   return (
     <div className="container">
@@ -37,6 +38,14 @@ const Board = () => {
         <div className={styles.categoryItem}>
           <span className={styles.categoryName}>공지사항</span>
           <span className={styles.categoryName}>특이사항</span>
+          {cateEmpty ? (
+            <div>카테고리를 생성해주세요</div>
+          ) : (
+            cateList &&
+            cateList.map(({ cate_name }) => {
+              return <div className={styles.categoryName}>{cate_name}</div>;
+            })
+          )}
         </div>
         <div onClick={openAddCategory}>
           <img src={add} alt="add button" className={styles.cateAddBtn} />
