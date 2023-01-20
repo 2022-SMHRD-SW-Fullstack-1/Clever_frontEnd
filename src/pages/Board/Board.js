7c4d6442f7d878af0dc98f0077098aa47c7ee382
ahaelim <limahae1015@gmail.com>
2023-01-20 17:24:36 +0900

    글쓰기 모달창

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -4,24 +4,31 @@ import AddBoardCate from "./Category/AddBoardCate";
 import styles from "./Board.module.scss";
 import add from "../../image/add.png";
 import axios from "axios";
+import WriteBoard from "./WriteBoard";
 
 const Board = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [showWriteModal, setShowWriteModal] = useState(false);
   const [groupInfo, setGroupInfo] = useState(location.state);
+  const mem_id = groupInfo.mem_id;
+  const [category, setCategory] = useState("");
+  const [writerInfo, setWriterInfo] = useState({
+    category: category,
+    mem_id: mem_id,
+  });
+  const [showAddCategory, setShowAddCategory] = useState(false);
   const [cateList, setCateList] = useState([]);
-  const [category, setCategory] = useState();
   const cateEmpty = cateList.length === 0;
 
+  // const handleWrite = () => {
+  //   navigate("/writeboard", { state: writerInfo });
+  // };
   const handleWrite = () => {
-    navigate("/writeboard");
+    setShowWriteModal(true);
   };
-  const [showAddCategory, setShowAddCategory] = useState(false);
   const openAddCategory = () => {
     setShowAddCategory(true);
-  };
-  const handleCategory = () => {
-    console.log();
   };
 
   useEffect(() => {
@@ -29,6 +36,8 @@ const Board = () => {
       .post("/board/getcategory", groupInfo)
       .then((res) => {
         setCateList(res.data);
+        console.log(res.data);
+        setCategory(res.data[0].cate_seq);
       })
       .catch((err) => {
         console.log(err);
@@ -43,14 +52,18 @@ const Board = () => {
             <div>카테고리를 생성해주세요</div>
           ) : (
             cateList &&
-            cateList.map(({ cate_name, cate_seq }) => {
+            cateList.map((item, idx) => {
               return (
                 <div
-                  className={styles.categoryName}
-                  key={cate_seq}
-                  onClick={handleCategory}
+                  className={
+                    item.cate_seq === category
+                      ? styles.selected
+                      : styles.categoryName
+                  }
+                  key={idx}
+                  onClick={() => setCategory(item.cate_seq)}
                 >
-                  {cate_name}
+                  {item.cate_name}
                 </div>
               );
             })
@@ -71,6 +84,12 @@ const Board = () => {
       <button className={styles.writeBtn} onClick={handleWrite}>
         글 작성
       </button>
+      {showWriteModal && (
+        <WriteBoard
+          setShowWriteModal={setShowWriteModal}
+          writerInfo={writerInfo}
+        />
+      )}
     </div>
   );
 };
