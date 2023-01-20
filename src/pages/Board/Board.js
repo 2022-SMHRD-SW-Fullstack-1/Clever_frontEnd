c4c114ffc7af3e1209fda5532f295b8b072d0190
ahaelim <limahae1015@gmail.com>
2023-01-20 10:37:29 +0900

    카테고리 목록 값 받아오기

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import AddBoardCate from "./AddBoardCate";
 import styles from "./Board.module.scss";
+import add from "../../image/add.png";
+import axios from "axios";
 
 const Board = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [groupInfo, setGroupInfo] = useState(location.state);
+  const [cateList, setCateList] = useState([]);
+  const cateEmpty = cateList.length === 0;
+
   const handleWrite = () => {
     navigate("/writeboard");
   };
@@ -15,22 +20,33 @@ const Board = () => {
     setShowAddCategory(true);
   };
 
+  useEffect(() => {
+    axios
+      .post("/board/getcategory", groupInfo)
+      .then((res) => {
+        setCateList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [cateList]);
+
   return (
     <div className="container">
       <div className={styles.categoryContainer}>
         <div className={styles.categoryItem}>
           <span className={styles.categoryName}>공지사항</span>
           <span className={styles.categoryName}>특이사항</span>
-          <span className={styles.categoryName}>카테고리</span>
-
-          <button onClick={openAddCategory}>카테고리 추가</button>
-          {showAddCategory && (
-            <AddBoardCate
-              setShowAddCategory={setShowAddCategory}
-              groupInfo={groupInfo}
-            />
-          )}
         </div>
+        <div onClick={openAddCategory}>
+          <img src={add} alt="add button" className={styles.cateAddBtn} />
+        </div>
+        {showAddCategory && (
+          <AddBoardCate
+            setShowAddCategory={setShowAddCategory}
+            groupInfo={groupInfo}
+          />
+        )}
       </div>
       <div className={styles.boardContainer}>게시글 목록</div>
       <button className={styles.writeBtn} onClick={handleWrite}>
