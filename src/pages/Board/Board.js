729d2f8a61da1d85ebbf119c804641c3d37b4229
ahaelim <limahae1015@gmail.com>
2023-01-25 14:50:34 +0900

    게시글 상세보기

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -14,6 +14,7 @@ const Board = () => {
   const [groupInfo, setGroupInfo] = useState(location.state);
 
   const [category, setCategory] = useState("");
+  const [cateName, setCateName] = useState("");
   const [writerInfo, setWriterInfo] = useState({
     category: category,
     mem_id: groupInfo.mem_id,
@@ -70,7 +71,10 @@ const Board = () => {
                       : styles.categoryName
                   }
                   key={idx}
-                  onClick={() => setCategory(item.cate_seq)}
+                  onClick={() => {
+                    setCategory(item.cate_seq);
+                    setCateName(item.cate_name);
+                  }}
                 >
                   {item.cate_name}
                 </div>
@@ -90,7 +94,7 @@ const Board = () => {
       </div>
 
       <div className={styles.boardContainer}>
-        <BoardList writerInfo={infoRef} />
+        <BoardList writerInfo={infoRef} cateName={cateName} />
       </div>
       <button className={styles.writeBtn} onClick={handleWrite}>
         글 작성
--- src/pages/Board/BoardDetail/BoardDetail.js
+++ src/pages/Board/BoardDetail/BoardDetail.js
@@ -1,17 +1,43 @@
 import React from "react";
 import styles from "./BoardDetail.module.scss";
 
-const BoardDetail = () => {
+const BoardDetail = ({ setShowDetail, detailItem, category }) => {
+  const handleClose = () => {
+    setShowDetail(false);
+  };
+
   return (
     <>
       <div className={styles.modalContainer}>
         <div className={styles.modalBlock}>
-          <span className={styles.close}>&times;</span>
+          <span className={styles.close} onClick={handleClose}>
+            &times;
+          </span>
           <div className={styles.modalContents}>
-            <span className={styles.description}></span>
+            <span className={styles.description}>{category}</span>
+            <div className={styles.titleInput}>
+              <div className={styles.title}>{detailItem.notice_title}</div>
+              <div className={styles.writerInfo}>
+                <div className={styles.writer}>{detailItem.mem_name}</div>
+                <div>{detailItem.notice_dt}</div>
+              </div>
+            </div>
+            <div className={styles.contentInput}>
+              {detailItem.notice_photo !== null && (
+                <img
+                  className={styles.attachImage}
+                  src={
+                    process.env.PUBLIC_URL + "/image/" + detailItem.notice_photo
+                  }
+                ></img>
+              )}
 
-            <div className={styles.fileArea}></div>
+              <div>{detailItem.notice_content}</div>
+            </div>
           </div>
+          <button className={styles.backBtn} onClick={handleClose}>
+            목록으로 돌아가기
+          </button>
         </div>
       </div>
     </>
