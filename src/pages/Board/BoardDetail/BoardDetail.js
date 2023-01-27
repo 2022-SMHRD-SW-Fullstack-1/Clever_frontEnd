c1d44316a3d69dc524299ee64dba1fd0044b2927
ahaelim <limahae1015@gmail.com>
2023-01-27 14:42:52 +0900

    게시글 댓글 css, 입력창 초기화

--- src/pages/Board/BoardDetail/BoardDetail.js
+++ src/pages/Board/BoardDetail/BoardDetail.js
@@ -1,30 +1,10 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
+import Comment from "../Comment/Comment";
 import styles from "./BoardDetail.module.scss";
 
 const BoardDetail = ({ setShowDetail, detailItem, category }) => {
-  const [isValid, setIsValid] = useState(false);
-  const [commContent, setCommContent] = useState("");
-  const [inputValue, setInputValue] = useState({
-    mem_id: detailItem.mem_id,
-    notice_seq: detailItem.notice_seq,
-    com_content: commContent,
-  });
-
   const handleClose = () => {
     setShowDetail(false);
-  };
-  const handlePostComm = () => {
-    axios
-      .post("/board/comment/post", inputValue)
-      .then((res) => {
-        alert("댓글 작성 완료!");
-        setCommContent("");
-      })
-      .catch((err) => {
-        alert("작성 실패");
-        console.log(err);
-      });
   };
 
   return (
@@ -56,32 +36,7 @@ const BoardDetail = ({ setShowDetail, detailItem, category }) => {
 
               <div className={styles.content}>{detailItem.notice_content}</div>
             </div>
-            <div className={styles.commContainer}>
-              <div className={styles.commCount}>댓글 0</div>
-              <div>댓글목록</div>
-
-              <textarea
-                className={styles.commInput}
-                type="text"
-                placeholder="댓글을 입력하세요."
-                name="com_content"
-                value={commContent}
-                onChange={(e) => setCommContent(e.target.value)}
-                onKeyUp={(e) => {
-                  e.target.value.length > 0
-                    ? setIsValid(true)
-                    : setIsValid(false);
-                }}
-              ></textarea>
-
-              <button
-                className={styles.commPostBtn}
-                disabled={isValid ? false : true}
-                onClick={handlePostComm}
-              >
-                등록
-              </button>
-            </div>
+            <Comment detailItem={detailItem} />
           </div>
           <button className={styles.backBtn} onClick={handleClose}>
             목록으로 돌아가기
