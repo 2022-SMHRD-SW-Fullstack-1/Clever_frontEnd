80f8ccd62d0a75e0d7a2c18fc0bec2b0ddcb5694
ahaelim <limahae1015@gmail.com>
2023-01-27 17:58:56 +0900

    헤더 수정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,39 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../image/logo2.png";
 import styles from "./Header.module.scss";
+import bell from "../image/bell.png";
+import my from "../image/my.png";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.header}>
       <div className={styles.contents}>
         <div className={styles.logoContainer}>
-          <Link to="/">
+          <Link to="/group">
             <img src={logo} alt="logo image" className={styles.logo} />
           </Link>
         </div>
         <div className={styles.navigation}>
-          <ul>
-            <li>
-              <a href="/todolist"> 할 일</a>
-            </li>
-            <li>
-              <a href="/calendar"> 일정</a>
-            </li>
-            <li>
-              <a href="/group"> 그룹</a>
-            </li>
-            <li>
-              <a href="/join">회원가입</a>
-            </li>
-            <li>
-              <a href="/">로그인</a>
-            </li>
-          </ul>
+          <div className={styles.content}>
+            <Link to="/todolist">할 일</Link>
+          </div>
+          <div className={styles.content}>
+            <Link to="/calendar">일정</Link>
+          </div>
+          <div className={styles.content}>
+            <Link to="/board">전달사항</Link>
+          </div>
+          <div className={styles.content}>멤버</div>
         </div>
         <div className={styles.otherContainer}>
-          <button>알림</button>
-          <button>마이페이지</button>
+          <img src={bell} alt="alert" className={styles.icon} />
+          <img src={my} alt="my page" className={styles.icon} />
         </div>
       </div>
     </div>
@@ -41,3 +38,20 @@ const Header = () => {
 };
 
 export default Header;
+
+{
+  /* <div className={styles.contents}>
+        <div className={styles.logoContainer}>
+          
+        </div>
+        <div className={styles.navigation}>
+          <div className={styles.content}>할 일</div>
+          <div className={styles.content}>일정</div>
+          <div className={styles.content}>전달사항</div>
+        </div>
+        <div className={styles.otherContainer}>
+          <img src={bell} alt="alert" className={styles.icon} />
+          <img src={my} alt="my page" className={styles.icon} />
+        </div>
+      </div> */
+}
